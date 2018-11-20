d438d1d47e00b299987bfde152c19331b0e0deb8
LouisJackman <LouisJackman@users.noreply.github.com>
2018-11-20 23:07:34 +0000

    Embed enough from the host to allow basic drawing

--- embedder/embed.js
+++ embedder/embed.js
@@ -1,18 +1,58 @@
-{
-    if (!("WebAssembly" in window)) {
-        throw new Error("WebAssembly not supported.");
-    }
+const { freeze } = Object;
 
-    const imports = {
-        console: {
-            log: (...args) => {
-                console.log(...args);
-            },
-        },
-    };
 
-    fetch('main.wasm')
-        .then(response => response.arrayBuffer())
-        .then(wasm => WebAssembly.instantiate(wasm, imports));
+if (!("WebAssembly" in window)) {
+    throw new Error("WebAssembly not supported.");
 }
 
+
+const consoleImports = {
+    log: console.log.bind(console),
+};
+
+
+const colours = new Map([
+    [0, "red"],
+    [1, "black"],
+    [2, "white"],
+    [3, "beige"],
+]);
+
+const colourImports = { };
+for (const [key, value] of colours.entries()) {
+    colourImports[value] = key;
+}
+freeze(colourImports);
+
+
+const canvasElement = document.querySelector("canvas.main");
+const context = canvasElement.getContext("2d");
+
+const canvas = {};
+
+canvas.setFillStyle = colourIndex => {
+    const colour = colours.get(colourIndex);
+    context.fillStyle = colour;
+};
+
+canvas.setStrokeStyle = colourIndex => {
+    const colour = colours.get(colourIndex);
+    context.strokeStyle = colour;
+};
+
+canvas.fillRect = (x, y, width, height) => {
+    context.fillRect(x, y, width, height);
+};
+
+freeze(canvas);
+
+
+const imports = {
+    colours: colourImports,
+    console: consoleImports,
+    canvas,
+};
+
+fetch('main.wasm')
+    .then(response => response.arrayBuffer())
+    .then(wasm => WebAssembly.instantiate(wasm, imports));
