0aa15924b371ac710a1de7a6ae7b82167b18808b
LouisJackman <LouisJackman@users.noreply.github.com>
2018-11-22 07:44:58 +0000

    Cleanup embedder code

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 I wanted to learn both WebAssembly and Backgammon, hence this project.
 
-Requires the following:
+It requires the following:
 
 * Make
-* POSIX Shell, a.k.a. a Bourne-compatible shell.
+* POSIX Shell, i.e. a Bourne-compatible shell.
 * The [WebAssembly Binary Toolkit](https://github.com/WebAssembly/wabt)'s programs in `$PATH`.
 
 Run `make help` for the available tasks for building and running the game.
--- embedder/embed.js
+++ embedder/embed.js
@@ -1,58 +1,79 @@
 const { freeze } = Object;
 
-
-if (!("WebAssembly" in window)) {
-    throw new Error("WebAssembly not supported.");
-}
-
-
-const consoleImports = {
+const consoleImports = freeze({
     log: console.log.bind(console),
+});
+
+const checkForWebAssembly = () => {
+    if (!("WebAssembly" in window)) {
+        throw new Error("WebAssembly not supported.");
+    }
 };
 
+const createColourImports = () => {
+    const colours = new Map([
+        [0, "red"],
+        [1, "black"],
+        [2, "white"],
+        [3, "beige"],
+    ]);
 
-const colours = new Map([
-    [0, "red"],
-    [1, "black"],
-    [2, "white"],
-    [3, "beige"],
-]);
+    const colourImports = { };
+    for (const [key, value] of colours.entries()) {
+        colourImports[value] = key;
+    }
 
-const colourImports = { };
-for (const [key, value] of colours.entries()) {
-    colourImports[value] = key;
-}
-freeze(colourImports);
-
-
-const canvasElement = document.querySelector("canvas.main");
-const context = canvasElement.getContext("2d");
-
-const canvas = {};
-
-canvas.setFillStyle = colourIndex => {
-    const colour = colours.get(colourIndex);
-    context.fillStyle = colour;
+    return [
+        colours,
+        colourImports,
+    ];
 };
 
-canvas.setStrokeStyle = colourIndex => {
-    const colour = colours.get(colourIndex);
-    context.strokeStyle = colour;
+const createCanvas = (colours) => {
+    const canvasElement = document.querySelector("canvas.main");
+    const context = canvasElement.getContext("2d");
+
+    const canvas = {};
+
+    canvas.setFillStyle = colourIndex => {
+        const colour = colours.get(colourIndex);
+        context.fillStyle = colour;
+    };
+
+    canvas.setStrokeStyle = colourIndex => {
+        const colour = colours.get(colourIndex);
+        context.strokeStyle = colour;
+    };
+
+    canvas.fillRect = (x, y, width, height) => {
+        context.fillRect(x, y, width, height);
+    };
+
+    return canvas;
 };
 
-canvas.fillRect = (x, y, width, height) => {
-    context.fillRect(x, y, width, height);
+const instantiateWebAssembly = imports =>
+    fetch('main.wasm')
+        .then(response => response.arrayBuffer())
+        .then(wasm => WebAssembly.instantiate(wasm, imports));
+
+const main = () => {
+    checkForWebAssembly();
+
+    const [colours, colourImports] = createColourImports();
+    freeze(colours);
+    freeze(colourImports);
+
+    const canvas = createCanvas(colours);
+    freeze(canvas);
+
+    const imports = freeze({
+        colours: colourImports,
+        console: consoleImports,
+        canvas,
+    });
+
+    instantiateWebAssembly(imports);
 };
 
-freeze(canvas);
-
-
-const imports = {
-    colours: colourImports,
-    console: consoleImports,
-    canvas,
-};
-
-fetch('main.wasm')
-    .then(response => response.arrayBuffer())
-    .then(wasm => WebAssembly.instantiate(wasm, imports));
+main();
